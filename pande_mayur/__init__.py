7d0f80cb6bac2c596b4eb4281e1c076188773935
mayur <mayurpande.uk@gmail.com>
2020-03-13 13:48:05 +0100

    imported models

--- pande_mayur/__init__.py
+++ pande_mayur/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from .views.address import address
 from .extensions import db, migrate
-from .models import *
+from .models.models import *
+from .config import Config
 
 
 def create_app():
     """Create app function to handle instantiation of Flask app and to be able to use outside of context if needed"""
 
     app = Flask(__name__)
+
+    # set config for app from Config class
+    app.config.from_object(Config)
 
     # init db on app
     db.init_app(app)
