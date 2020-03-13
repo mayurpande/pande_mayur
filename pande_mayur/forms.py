a7324806da7f5b2addef868a57deed30443d78b1
mayur <mayurpande.uk@gmail.com>
2020-03-13 18:45:13 +0100

    checked length in custom validator rules to allow for no input

--- pande_mayur/forms.py
+++ pande_mayur/forms.py
@@ -4,43 +4,41 @@ from wtforms.validators import InputRequired, Length, Email, ValidationError
 
 
 class TelephoneForm(Form):
-
     """class to hold form fields for country code and number"""
 
     country_code = StringField('Country Code')
     number = StringField('Number')
 
     def validate_country_code(self, field):
+
         """Method to create a custom validation rule to check for length and type"""
 
-        try:
-            # checks if instance type is of field data is int (once type casted to int)
-            True if isinstance(int(field.data), int) else False
-        except ValueError:
-            raise ValidationError('Not a numeric number (it needs to be 2 or 3 digits).')
+        if len(field.data) > 0:
+            try:
+                # checks if instance type is of field data is int (once type casted to int)
+                True if isinstance(int(field.data), int) else False
+            except ValueError:
+                raise ValidationError('Not a numeric number (it needs to be 2 or 3 digits).')
 
-        print(len(field.data))
-        if len(field.data) != 2 and len(field.data) != 3:
-            # raise wtf custom validation error, with custom message
-            raise ValidationError('You have not entered 2 or 3 digits.')
-
+            if len(field.data) != 2 and len(field.data) != 3:
+                # raise wtf custom validation error, with custom message
+                raise ValidationError('You have not entered 2 or 3 digits.')
 
     def validate_number(self, field):
 
         """Method to create a custom validation rule to check for length and type"""
 
-        try:
-            True if isinstance(int(field.data), int) else False
-        except ValueError:
-            raise ValidationError('Not a numeric number (it needs to be 2 or 3 digits).')
+        if len(field.data) > 0:
+            try:
+                True if isinstance(int(field.data), int) else False
+            except ValueError:
+                raise ValidationError('Not a numeric number (it needs to be 2 or 3 digits).')
 
-        if len(field.data) != 9:
-            raise ValidationError('You have not entered 9 digits.')
-
+            if len(field.data) != 9:
+                raise ValidationError('You have not entered 9 digits.')
 
 
 class ContactForm(FlaskForm):
-
     """Flask-WTF form to create form fields to use in template"""
 
     # created form fields using respective validators
@@ -50,4 +48,3 @@ class ContactForm(FlaskForm):
     email = StringField('Email', validators=[Email(), Length(max=300)])
     # Created field enclosure object to handle child objects
     phone = FormField(TelephoneForm)
-
