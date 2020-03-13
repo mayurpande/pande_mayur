e78f69a063aaf47226edcbe22d3a45833000cbfa
mayur <mayurpande.uk@gmail.com>
2020-03-13 15:15:38 +0100

    adjusted captilisation for form field names

--- pande_mayur/forms.py
+++ pande_mayur/forms.py
@@ -7,8 +7,8 @@ class TelephoneForm(Form):
 
     """class to hold form fields for country code and number"""
 
-    country_code = IntegerField('country code', validators=[InputRequired(), Length(min=2, max=2)])
-    number = IntegerField('number', validators=[InputRequired(), Length(min=9, max=9)])
+    country_code = IntegerField('Country Code', validators=[InputRequired(), Length(min=2, max=2)])
+    number = IntegerField('Number', validators=[InputRequired(), Length(min=9, max=9)])
 
 
 class ContactForm(FlaskForm):
@@ -18,8 +18,8 @@ class ContactForm(FlaskForm):
     # created form fields using respective validators
     first_name = StringField('First name', validators=[InputRequired(), Length(max=50)])
     last_name = StringField('Last name', validators=[InputRequired(), Length(max=50)])
-    address = StringField('address', validators=[Length(max=300)])
-    email = StringField('email', validators=[Email(), Length(max=300)])
+    address = StringField('Address', validators=[Length(max=300)])
+    email = StringField('Email', validators=[Email(), Length(max=300)])
     # Created field enclosure object to handle child objects
     phone = FormField(TelephoneForm)
 
