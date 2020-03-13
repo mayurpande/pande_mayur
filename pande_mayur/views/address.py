8f6fcaa3aea4d274a58d32f72766232a8c1c9972
mayur <mayurpande.uk@gmail.com>
2020-03-13 19:42:02 +0100

    called Contact model added and committed it db

--- pande_mayur/views/address.py
+++ pande_mayur/views/address.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 from pande_mayur.forms import ContactForm
+from pande_mayur.models.models import Contact
+from pande_mayur.extensions import db
 
 # created blueprint for main address routes
 address = Blueprint('address', __name__)
@@ -8,16 +10,27 @@ address = Blueprint('address', __name__)
 @address.route('/', methods=['GET', 'POST'])
 def index():
 
-    """main index route for address book which will loads the form"""
+    """main index route for address book which will loads the form and handles post requests to save to db"""
 
     # instantiated ContactForm class
     contact = ContactForm()
 
     if contact.validate_on_submit():
-        return '<h1>Country code {}, area code {}, number {}</h1>'.format(contact.country_code.data,
-                                                                          contact.home_phone.area_code.data,
-                                                                          contact.home_phone.number.data
-                                                                          )
+
+        # create a Contact model object to insert to db
+        contact_data = Contact(
+            firstname=contact.first_name.data,
+            lastname=contact.last_name.data,
+            address=contact.address.data,
+            email=contact.email.data,
+            phone=contact.phone['country_code'].data + contact.phone['number'].data
+        )
+
+        # add the new object to the session and then commit it
+        db.session.add(contact_data)
+        db.session.commit()
+
+
 
 
     return render_template('index.html', form=contact)
