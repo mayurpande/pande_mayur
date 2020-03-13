e5a942344e34785f0e03197a0d7248f92ab64741
mayur <mayurpande.uk@gmail.com>
2020-03-13 15:15:13 +0100

    added random secret key

--- pande_mayur/config.py
+++ pande_mayur/config.py
@@ -8,3 +8,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
+    SECRET_KEY = '\x1b\x01\x8a&\x9c\xc0\xcd\x9d\xc1{\x88Y4\xc6\xac\x00\x8c\xc6\xcc&\xc7reF'
