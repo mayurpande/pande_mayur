203e028dc06b5b4ace81ad70c89386202a49a1b8
mayur <mayurpande.uk@gmail.com>
2020-03-16 11:36:55 +0100

    created blueprint for xml created route with get variable, set path

--- pande_mayur/views/xml_file.py
+++ pande_mayur/views/xml_file.py
@@ -1,6 +1,21 @@
 from flask import Blueprint, render_template, flash, redirect, request
 from pande_mayur.models.models import Contact
 from pande_mayur.extensions import db
+from pathlib import Path
+import os
 
 # created blueprint for handling xml file route
-xml_file = Blueprint('xml_input', __name__)
+xml_blueprint = Blueprint('xml_input', __name__)
+
+
+# defined route, passed get variable to handle filename
+@xml_blueprint.route('/xml/<xml>', methods=['GET','POST'])
+def xml_file(xml):
+
+    # set path string (subject to change depending on which directory the end user wants to put the xml file
+    path = str(Path(__file__).resolve().parent.parent)
+
+    if os.path.exists(path + '/' + xml):
+        print(path + '/' + xml)
+
+    return 'hello'
